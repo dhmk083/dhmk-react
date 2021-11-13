d09f6f27996e3f535fca1c8cbc31eb6da60118e4
dhmk083 <dhmk083@gmail.com>
2021-11-13 15:52:01 +0300

    ignored /esm folder

--- esm/react.d.ts
+++
@@ -1,5 +0,0 @@
-import React from "react";
-export declare function Render({ children, }: {
-    children: () => React.ReactElement | null;
-}): React.ReactElement<any, string | React.JSXElementConstructor<any>> | null;
-export declare function effect(self: any, fn: any, deps?: () => ReadonlyArray<any> | undefined): () => any;
